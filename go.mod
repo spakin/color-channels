cd9c55bce3e4f1edf5c3035d7ed039443a8aa46a
Scott Pakin <scott+gpg@pakin.org>
2022-04-23 20:16:15 -0600

    Produce an initial draft of the program
    At this point, merge-channels can merge only HCL channels.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/spakin/merge-channels
+
+go 1.17
+
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0
+	github.com/spakin/netpbm v1.3.0
+)
---
+++ merge-channels.go
@@ -0,0 +1,126 @@
+/*
+merge-channels merges separately provided channels to produce a new image.
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"os"
+
+	"github.com/lucasb-eyer/go-colorful"
+	_ "github.com/spakin/netpbm"
+)
+
+// notify is used to output error messages.
+var notify *log.Logger
+
+// ReadImage reads an arbitrary image from a named file.  It aborts on error.
+func ReadImage(fn string) image.Image {
+	// Read the input image.
+	r, err := os.Open(fn)
+	if err != nil {
+		notify.Fatal(err)
+	}
+	defer r.Close()
+	img, _, err := image.Decode(r)
+	if err != nil {
+		notify.Fatal(err)
+	}
+	return img
+}
+
+// ReadGrayscaleImage reads a grayscale image from a named file.  It aborts on
+// error.
+func ReadGrayscaleImage(fn string) *image.Gray {
+	// Read a generic image.
+	img := ReadImage(fn)
+
+	// Convert the image to grayscale.
+	bnds := img.Bounds()
+	gray := image.NewGray(bnds)
+	for y := bnds.Min.Y; y < bnds.Max.Y; y++ {
+		for x := bnds.Min.X; x < bnds.Max.X; x++ {
+			gray.Set(x, y, img.At(x, y))
+		}
+	}
+	return gray
+}
+
+// MergeHCL merges hue, chroma, and lightness channels into a single image.
+func MergeHCL(imgs []*image.Gray) image.Image {
+	bnds := imgs[0].Bounds()
+	merged := image.NewNRGBA(bnds)
+	for y := bnds.Min.Y; y < bnds.Max.Y; y++ {
+		for x := bnds.Min.X; x < bnds.Max.X; x++ {
+			h := float64(imgs[0].GrayAt(x, y).Y) * 360.0 / 255.0
+			c := float64(imgs[1].GrayAt(x, y).Y) / 255.0
+			l := float64(imgs[2].GrayAt(x, y).Y) / 255.0
+			clr := colorful.Hcl(h, c, l).Clamped()
+			merged.Set(x, y, clr)
+		}
+	}
+	return merged
+}
+
+// WritePNG writes an arbitrary image to a named PNG file.  If the file is "",
+// write to standard output.
+func WritePNG(fn string, img image.Image) error {
+	var w io.Writer = os.Stdout
+	if fn != "" {
+		f, err := os.Create(fn)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		w = f
+	}
+	err := png.Encode(w, img)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	// Read three images from the command line.
+	notify = log.New(os.Stderr, os.Args[0]+": ", 0)
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] <hue.png> <chroma.png> <lightness.png>\n", os.Args[0])
+		fmt.Fprint(flag.CommandLine.Output(), "Options:\n\n")
+		flag.PrintDefaults()
+	}
+	outName := flag.String("o", "", "Name of output stereogram file (default standard output)")
+	flag.Parse()
+	if flag.NArg() < 3 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Read the three color-channel images.
+	channels := make([]*image.Gray, 0, 4)
+	for _, fn := range flag.Args() {
+		g := ReadGrayscaleImage(fn)
+		channels = append(channels, g)
+	}
+
+	// Ensure that all channels have the same bounds.
+	bnds := channels[0].Bounds()
+	for _, g := range channels {
+		if g.Bounds() != bnds {
+			notify.Fatal("All input images must have the same dimensions")
+		}
+	}
+
+	// Merge the channels and write the result to a file.
+	merged := MergeHCL(channels)
+	err := WritePNG(*outName, merged)
+	if err != nil {
+		notify.Fatal(err)
+	}
+}
